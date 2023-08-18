9cd0300f56dde9ba5e09fab481d886bc629e7c86
Styles <139181994+stylescode@users.noreply.github.com>
2023-08-18 12:28:48 -0700

    update playRound function to compare choices

--- javascript.js
+++ javascript.js
@@ -1,9 +1,10 @@
 function getComputerChoice() {
   // computer needs to randomly select between R/P/S
   // randomly select a number
-  // apply choice to computer player based on number
+  // apply choice based on number
   let selection
   let number = Math.random();
+  console.log(number);
   if (number < (1 / 3)) {
     selection = "ROCK";
   } else if (number < (2 / 3)) {
@@ -11,21 +12,31 @@ function getComputerChoice() {
   } else {
     selection = "SCISSORS";
   }
-  console.log("Computer chose: ")
+  console.log("Computer chose: " + selection)
   return selection;
 }
 
 function getPlayerChoice() {
   // prompt for user input
   // make sure user input is a valid choice
-  let selection = prompt("Choose ROCK, PAPER, or SCISSORS").toUpperCase();
-  if (selection != "ROCK" || selection != "PAPER" || selection != "SCISSORS") {
-    // user needs to try again
+  let selection;
+  while (selection != "ROCK" && selection != "PAPER" && selection != "SCISSORS") {
+    selection = prompt("Choose ROCK, PAPER, or SCISSORS").toUpperCase();
   }
   console.log("You chose: " + selection);
+  return selection;
 }
 
 function playRound(playerSelection, computerSelection) {
+  // compare player and computer choices
+  console.log(playerSelection, computerSelection);
+  if (playerSelection === computerSelection) {
+    console.log("it's a tie");
+  } else if ((playerSelection === "ROCK" && computerSelection === "PAPER") || (playerSelection === "PAPER" && computerSelection === "SCISSORS") || (playerSelection === "SCISSORS" && computerSelection ===  "ROCK")) {
+    console.log("computer wins this round")
+  } else {
+    console.log("you win this round");
+  }
 
 }
 
